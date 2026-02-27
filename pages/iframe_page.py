70b11e814381c81abd1dad6dca105558c42d5a24
Micah Breitenstein <micah.breitenstein@gmail.com>
2026-02-26 19:32:27 -0800

    Add iframe page

--- pages/iframe_page.py
+++ pages/iframe_page.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from dataclasses import dataclass
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
@@ -30,22 +28,22 @@ class IFramePage:
             EC.presence_of_element_located(self.IFRAME)
         )
 
-    def switch_to_editor(self) -> None:
+    def _switch_to_editor(self) -> None:
         iframe = self.driver.find_element(*self.IFRAME)
         self.driver.switch_to.frame(iframe)
 
-    def switch_to_default(self) -> None:
+    def _switch_to_default(self) -> None:
         self.driver.switch_to.default_content()
 
     def get_editor_text(self) -> str:
-        self.switch_to_editor()
+        self._switch_to_editor()
         text = self.driver.find_element(*self.EDITOR_BODY).text
-        self.switch_to_default()
-        return text
+        self._switch_to_default()
+        return " ".join(text.split())
 
     def set_editor_text(self, text: str) -> None:
-        self.switch_to_editor()
+        self._switch_to_editor()
         body = self.driver.find_element(*self.EDITOR_BODY)
         body.clear()
         body.send_keys(text)
-        self.switch_to_default()
+        self._switch_to_default()
